413a0025a8dc884b8db0ccc64bdd2ad706dde5cd
Gathers <cmgathers.2017@mymail.jcsu.edu>
2018-02-15 13:03:54 -0500

    first commit

---
+++ src/EvenorOdd.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class EvenorOdd {
+
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	Scanner scan=new Scanner(System.in);
+	System.out.println("Please enter a number");
+	int x=scan.nextInt();
+		
+if((x%2)==0)
+System.out.println("True");	
+else
+System.out.println("False");
+	}
+
+}
